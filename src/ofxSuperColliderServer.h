2f2907e36c6c7988bd2975f4284e4cccd9a5e59f
Satoru Higa <satoruhiga@gmail.com>
2010-08-20 21:56:43 +0900

    synthdef -> synthdefs

--- src/ofxSuperColliderServer.h
+++ src/ofxSuperColliderServer.h
@@ -5,5 +5,5 @@
 class ofxSuperColliderServer
 {
 public:
-	static void init(int port = 57110, int samplerate = 44100, string synthdef_dir = "synthdef");
+	static void init(int port = 57110, int samplerate = 44100, string synthdef_dir = "synthdefs");
 };
